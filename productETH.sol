33a7a73ca1201c025ef3717087312fea824641c0
MAlasmari0 <78299760+MAlasmari0@users.noreply.github.com>
2021-02-23 16:34:52 +0300

    Update productETH.sol

--- productETH.sol
+++ productETH.sol
@@ -6,12 +6,8 @@ pragma solidity >=0.6.0 <0.8.1;
  * @dev complete the functions
  */
 contract TechInsurance {
+
     
-    /** 
-     * Define two structs
-     * 
-     * 
-     */
     struct Product {
         uint productId;
         string productName;
@@ -23,17 +19,16 @@ contract TechInsurance {
         bool isValid;
         uint time;
     }
-    
-    
+
     mapping(uint => Product) public productIndex;
     mapping(address => mapping(uint => Client)) public client;
     
     uint productCounter;
     
-    // address payable insOwner;
-    // constructor(address payable _insOwner) public{
-    //     insOwner = _insOwner;
-    // }
+    address payable public insOwner ;
+    constructor(address payable _insOwner) public{
+        insOwner = _insOwner;
+    }
  
     function addProduct(uint _productId, string memory _productName, uint _price ) public {
         Product memory newProduct = Product (_productId, _productName, _price, true);
@@ -41,26 +36,31 @@ contract TechInsurance {
     }
     
     
-    function changeFalse(uint _productIndex) public {
+    function changeFalse(uint _productIndex) public onlyOwner {
         productIndex[_productIndex].offered = false;
-
     }
     
-    function changeTrue(uint _productIndex) public {
+    function changeTrue(uint _productIndex) public onlyOwner{
         productIndex[_productIndex].offered = true;
-
     }
     
-    function changePrice(uint _productIndex, uint _price) public {
+    modifier onlyOwner {
+      require(msg.sender == insOwner, "you are not the owner" );
+      _;
+      revert("do not run this function again it cost you some fees");
+     }
+ 
+     
+    function changePrice(uint _productIndex, uint _price) public onlyOwner{
         productIndex[_productIndex].price = _price;
-
-
     }
 
-    function clientSelect(uint _productIndex) public payable {
+    function buyInsurance(uint _productIndex) public payable {
+        require(productIndex[_productIndex].offered == true, "Tha Insurance is not available");
         Client memory newClient = Client (true, block.timestamp);
         client[msg.sender][_productIndex] =   newClient ;
-        
+        require(msg.value  == productIndex[_productIndex].price, "check the amount of the Insurance");
     } 
+
     
 }
